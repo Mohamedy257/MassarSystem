e8a5b886317771665c2603e8ee48b3b9b21bd947
Mohamedy257 <mohamed_y257@hotmail.com>
2018-07-31 21:31:48 +0300

    Initial Commit

---
+++ MassarSystem/App_Code/Startup.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(MassarSystem.Startup))]
+namespace MassarSystem
+{
+    public partial class Startup {
+        public void Configuration(IAppBuilder app) {
+            ConfigureAuth(app);
+        }
+    }
+}
